7fad25e599059054c03cfee25c0a76848ae9370c
Bojan <bojan.filipovic.15@singimail.rs>
2019-05-09 22:21:13 +0200

    Osnovni fajlovi za rad sa bazom podataka

--- index.php
+++ index.php
@@ -1,1 +1,19 @@
 <?php
+
+    require_once('DatabaseConfiguration.php');
+    require_once('DatabaseConnection.php');
+    require_once('models/UserModel.php');
+
+
+    $databaseConfiguration = new DatabaseConfiguration('localhost', 'root', '', 'auction_project');
+    $databaseConnection = new databaseConnection($databaseConfiguration);
+    
+
+    $userModel = new UserModel($databaseConnection);
+
+    $user = $userModel->getByUsername('user_one');
+
+
+    print_r($user);
+
+ 
---
+++ models/UserModel.php
@@ -0,0 +1,44 @@
+<?php 
+    class UserModel {
+        private $dbc;
+
+        public function __construct(DatabaseConnection &$dbc) {
+            $this->dbc = $dbc;
+        }
+
+        public function getById(int $userId) {
+            $sql = 'SELECT * FROM user WHERE user_id =?;';
+            $prep = $this->dbc->getConnection()->prepare($sql);
+            $res = $prep->execute([$user_id]);
+
+            $user = [];
+            if($res) {
+                $user = $prep->fetch(PDO::FETCH_OBJ);
+            }
+            return $user;
+        }
+
+        public function getAll(): array {
+            $sql = 'SELECT * FROM user; ';
+            $prep = $this->dbc->getConnection()->prepare($sql);
+            $res = $prep->execute();
+
+            $user = [];
+            if($res) {
+                $user = $prep->fetchAll(PDO::FETCH_OBJ);
+            }
+            return $user;
+        }
+
+        public function getByUsername(string $username) {
+            $sql = 'SELECT * FROM user WHERE username = ?;';
+            $prep = $this->dbc->getConnection()->prepare($sql);
+            $res = $prep->execute([$username]);
+
+            $user = [];
+            if($res) {
+                $user = $prep->fetch(PDO::FETCH_OBJ);
+            }
+            return $user;
+        }
+    }
